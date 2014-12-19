1dd1f9dbc531dd7dfebf369a8c3381db169dccfd
liberdaa <liberdaa@CZEPL20213.ad.trw.com>
2014-12-19 14:13:06 +0100

    [hal] SDL2 is implemented, the base test for create window is working. Remove old SDL1.x library files.

--- bc_pc_emu_c/src/bc_pc_emu_c.c
+++ bc_pc_emu_c/src/bc_pc_emu_c.c
@@ -1,4 +1,5 @@
 /*
+ * bc_pc_emu_c.c
  *
  *
  *
@@ -6,14 +7,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "bc_pc_emu_c.h"
 #include <SDL.h>
+#include "bc_pc_emu_c.h"
 
-// SDL test
-#define WIDTH 640
-#define HEIGHT 480
-#define BPP 4
-#define DEPTH 32
 
 
 void APP_mainLoop(void)
@@ -21,29 +17,26 @@ void APP_mainLoop(void)
 
 }
 
-//int main(void) - with this main, I get error as below.
-//c:/mingw/bin/../lib/gcc/mingw32/4.5.0/../../../libmingw32.a(main.o):main.c:(.text+0xd2): undefined reference to `WinMain@16'
-//collect2: ld returned 1 exit status
-//make: *** [bc_pc_emu_c.exe] Error 1
 
-int main(int argc, char* argv[])
-{
-	//puts("Hello");
-	printf("Hello. This is SDL test \n");
+int main(int argc, char* argv[]) {
+    // Start SDL2
+    SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Surface *screen;
-    //SDL_Event event;
+    // Create a Window in the middle of the screen
+    SDL_Window *window = 0;
 
-    //int keypress = 0;
-    //int h=0;
+    window = SDL_CreateWindow("Hello World!",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              640, 480,
+                              SDL_WINDOW_SHOWN);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0 ) return 1;
+    // Delay so that we can see the window appear
+    SDL_Delay(2000);
 
-//    if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_FULLSCREEN|SDL_HWSURFACE)))
-//    {
-        SDL_Quit();
-//        return 1;
-//    }
+    // Cleanup and Quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-	return EXIT_SUCCESS;
+    return 0;
 }
